10767e9a7ca3adc9112e4d55319578fbae7ab326
Leonp24 <prastikomantap@gmail.com>
2024-05-11 13:39:35 +0700

    upload

---
+++ lib/themes/themes.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// buat variabel yang digunakan dalam format desain
+
+// warna
+Color primaryColor = Color(0XFFFFD422);
+Color secondaryColor = Color(0XFF964B22);
+Color blackColor = Color(0XFF000000);
+Color greyColor = Color(0XFF828282);
+Color greenColor = Color(0XFF26FF22);
+Color redColor = Color(0XFFD23030);
+Color whiteColor = Color(0XFFFFFFFF);
+Color bgColor = Color(0XFFF4F3EF);
+
+// text
+TextStyle primaryTextstyle = GoogleFonts.outfit(
+  color: primaryColor
+);
+TextStyle secondaryTextstyle = GoogleFonts.outfit(
+  color: secondaryColor
+);
+TextStyle blackTextstyle = GoogleFonts.outfit(
+  color: blackColor
+);
+TextStyle greyTextstyle = GoogleFonts.outfit(
+  color: greyColor
+);
+TextStyle greenTextstyle = GoogleFonts.outfit(
+  color: greenColor
+);
+TextStyle redTextstyle = GoogleFonts.outfit(
+  color: redColor
+);
+TextStyle whiteTextstyle = GoogleFonts.outfit(
+  color: whiteColor
+);
+TextStyle splashTextstyle1 = GoogleFonts.slackey(
+  color: blackColor
+);
+TextStyle splashTextstyle2 = GoogleFonts.slackey(
+  color: secondaryColor
+);
